7d7bdb9633b47363286fb15b655b3513130f5101
shishir bhandari <motivate23a@gmail.com>
2023-04-08 09:09:28 -0700

    Minor Fixed

--- src/Admin/components/sidebar.js
+++ src/Admin/components/sidebar.js
@@ -6,49 +6,7 @@ function Sidebar() {
     <div>
       <div className="relative hidden h-screen my-4 ml-4 shadow-lg lg:block w-80">
         <div className="h-full bg-white rounded-2xl ">
-          <div className="flex items-center justify-center pt-6">
-            <svg
-              width={35}
-              height={30}
-              viewBox="0 0 256 366"
-              version="1.1"
-              preserveAspectRatio="xMidYMid"
-            >
-              <defs>
-                <linearGradient
-                  x1="12.5189534%"
-                  y1="85.2128611%"
-                  x2="88.2282959%"
-                  y2="10.0225497%"
-                  id="linearGradient-1"
-                >
-                  <stop stopColor="#FF0057" stopOpacity="0.16" offset="0%" />
-                  <stop stopColor="#FF0057" offset="86.1354%" />
-                </linearGradient>
-              </defs>
-              <g>
-                <path
-                  d="M0,60.8538006 C0,27.245261 27.245304,0 60.8542121,0 L117.027019,0 L255.996549,0 L255.996549,86.5999776 C255.996549,103.404155 242.374096,117.027222 225.569919,117.027222 L145.80812,117.027222 C130.003299,117.277829 117.242615,130.060011 117.027019,145.872817 L117.027019,335.28252 C117.027019,352.087312 103.404567,365.709764 86.5997749,365.709764 L0,365.709764 L0,117.027222 L0,60.8538006 Z"
-                  fill="#001B38"
-                />
-                <circle
-                  fill="url(#linearGradient-1)"
-                  transform="translate(147.013244, 147.014675) rotate(90.000000) translate(-147.013244, -147.014675) "
-                  cx="147.013244"
-                  cy="147.014675"
-                  r="78.9933938"
-                />
-                <circle
-                  fill="url(#linearGradient-1)"
-                  opacity="0.5"
-                  transform="translate(147.013244, 147.014675) rotate(90.000000) translate(-147.013244, -147.014675) "
-                  cx="147.013244"
-                  cy="147.014675"
-                  r="78.9933938"
-                />
-              </g>
-            </svg>
-          </div>
+          <div className="flex items-center justify-center pt-6"></div>
           <nav className="mt-6">
             <div>
               <NavLink
@@ -66,7 +24,7 @@ function Sidebar() {
                     <path d="M1070 1178l306-564h-654l-306 564h654zm722-282q0 182-71 348t-191 286-286 191-348 71-348-71-286-191-191-286-71-348 71-348 191-286 286-191 348-71 348 71 286 191 191 286 71 348z" />
                   </svg>
                 </span>
-                <span className="mx-4 text-sm font-normal">Overview</span>
+                <span className="mx-4 text-sm font-normal">Dashboard</span>
               </NavLink>
               <NavLink
                 className="flex items-center justify-start w-full p-4 my-2 font-thin text-gray-500 uppercase transition-colors duration-200 hover:text-blue-500"
@@ -104,7 +62,7 @@ function Sidebar() {
                 </span>
                 <span className="mx-4 text-sm font-normal">My Patient</span>
               </NavLink>
-              <NavLink
+              {/* <NavLink
                 className="flex items-center justify-start w-full p-4 my-2 font-thin text-gray-500 uppercase transition-colors duration-200 hover:text-blue-500"
                 to="/payment"
               >
@@ -121,8 +79,8 @@ function Sidebar() {
                   </svg>
                 </span>
                 <span className="mx-4 text-sm font-normal">Payment</span>
-              </NavLink>
-              <NavLink
+              </NavLink> */}
+              {/* <NavLink
                 className="flex items-center justify-start w-full p-4 my-2 font-thin text-gray-500 uppercase transition-colors duration-200 hover:text-blue-500"
                 to="/schedule"
               >
@@ -141,8 +99,8 @@ function Sidebar() {
                 <span className="mx-4 text-sm font-normal">
                   Schedule Timings
                 </span>
-              </NavLink>
-              <NavLink
+              </NavLink> */}
+              {/* <NavLink
                 className="flex items-center justify-start w-full p-4 my-2 font-thin text-gray-500 uppercase transition-colors duration-200 hover:text-blue-500"
                 to="/blog"
               >
@@ -159,8 +117,8 @@ function Sidebar() {
                   </svg>
                 </span>
                 <span className="mx-4 text-sm font-normal">Blogs</span>
-              </NavLink>
-              <NavLink
+              </NavLink> */}
+              {/* <NavLink
                 className="flex items-center justify-start w-full p-4 my-2 font-thin text-gray-500 uppercase transition-colors duration-200 hover:text-blue-500"
                 to="/setting"
               >
@@ -177,7 +135,7 @@ function Sidebar() {
                   </svg>
                 </span>
                 <span className="mx-4 text-sm font-normal">Settings</span>
-              </NavLink>
+              </NavLink> */}
             </div>
           </nav>
         </div>
