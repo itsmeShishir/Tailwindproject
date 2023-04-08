6d3ec27658c0a2e7223452f615a00fe1ff222c0e
shishir bhandari <motivate23a@gmail.com>
2023-04-08 04:26:16 -0700

    Submiting the profile option

--- src/Admin/components/sidebar.js
+++ src/Admin/components/sidebar.js
@@ -52,7 +52,7 @@ function Sidebar() {
           <nav className="mt-6">
             <div>
               <NavLink
-                className="flex items-center justify-start w-full p-4 my-2 font-thin text-blue-500 uppercase transition-colors duration-200 border-r-4 border-blue-500 bg-gradient-to-r from-white to-blue-100 dark:from-gray-700 dark:to-gray-800"
+                className="flex items-center justify-start w-full p-4 my-2 font-thin text-gray-500 uppercase transition-colors duration-200 dark:text-gray-200 hover:text-blue-500"
                 to="/overview"
               >
                 <span className="text-left">
