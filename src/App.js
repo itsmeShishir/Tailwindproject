cd11e2e0ca55331c397061c6251218f37ccddc21
shishir bhandari <motivate23a@gmail.com>
2023-04-07 21:02:05 -0700

    Admin panel have been started

--- src/App.js
+++ src/App.js
@@ -8,10 +8,18 @@ import Login from "./pages/auth/Login/login.js";
 import Register from "./pages/auth/Register/register.js";
 import Single from "./pages/SingleBlogs/Single.js";
 import AboutUs from "./pages/Aboutus/Aboutus.js";
+import OverView from "./Admin/Overview/overview.js";
+import Payment from "./Admin/MyPatient/payment.js";
+import Message from "./Admin/message/Message.js";
+import Blog from "./Admin/Blog/Blog.js";
+import Appointment from "./Admin/Appointment/Appointment.js";
+import Schedule from "./Admin/Schedule/Schedule.js";
+import Setting from "./Admin/Setting/Setting.js";
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Frontend panel */}
         <Route path="/" element={<Home />} />
         <Route index element={<Home />} />
         <Route path="blogs" element={<Blogs />} />
@@ -20,6 +28,15 @@ function App() {
         <Route path="signup" element={<Register />} />
         <Route path="single" element={<Single />} />
         <Route path="aboutus" element={<AboutUs />} />
+        {/* Doctor Panel */}
+        <Route path="overview" element={<OverView />} />
+        <Route path="payment" element={<Payment />} />
+        <Route path="message" element={<Message />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="appointment" element={<Appointment />} />
+        <Route path="schedule" element={<Schedule />} />
+        <Route path="setting" element={<Setting />} />
+        {/* Page 404 */}
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
